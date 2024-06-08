46038c225e3fb5afa2e1e586fe7af3d2bec09223
Thomas Beer <thomas.beer04@outlook.com>
2024-06-08 23:42:36 +0100

    Join and member since comand

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from discord.ext import commands
 from intents import intents
 from dotenv import dotenv_values
 import logging
+import datetime
 
-handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+handler = logging.FileHandler(filename="logs/error.log", encoding="utf-8", mode="w")
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
@@ -14,11 +15,57 @@ async def on_ready():
     await bot.tree.sync()
 
 
-@bot.hybrid_command(name="ping")
-async def ping(ctx):
-    await ctx.send("Pong!")
+@bot.hybrid_command(name="play")
+async def play(ctx, playlist: str):
+    if not ctx.author.voice:
+        await ctx.send("You are not connected to a voice channel")
+        return
+    voice_channel = ctx.author.voice.channel
+    await voice_channel.connect()
+
+
+@bot.hybrid_command(name="join")
+async def join_date(ctx, member: discord.Member):
+    await ctx.send(
+        f"{member.mention} joined the server at <t:{member.joined_at.timestamp()}>"
+    )
+
+
+def format_time_delta(time: datetime.timedelta) -> str:
+    seconds = time.total_seconds()
+    years = int(seconds // 31536000)
+    seconds -= years * 31536000
+    months = int(seconds // 2592000)
+    seconds -= months * 2592000
+    weeks = int(seconds // 604800)
+    seconds -= weeks * 604800
+    days = int(seconds // 86400)
+    seconds -= days * 86400
+    time_list = [
+        i
+        for i in [
+            f"{years} years",
+            f"{months} months",
+            f"{weeks} weeks",
+            f"{days} days",
+        ]
+        if int(i[0]) != 0
+    ]
+    return (
+        ", ".join(time_list[:-1]) + " and " + time_list[-1]
+        if len(time_list) > 1
+        else time_list[0]
+    )
+
+
+@bot.hybrid_command(name="member-since")
+async def member_since(ctx, member: discord.Member):
+    difference = datetime.datetime.now(datetime.UTC) - member.joined_at
+    await ctx.send(
+        f"{member.mention} has been a member for {format_time_delta(difference)}."
+    )
 
 
 if __name__ == "__main__":
     config = dotenv_values()
-    bot.run(config["AUTH_TOKEN"], log_handler=handler, log_level=logging.DEBUG)
+    bot.run(config["AUTH_TOKEN"], log_handler=handler, log_level=logging.ERROR)
